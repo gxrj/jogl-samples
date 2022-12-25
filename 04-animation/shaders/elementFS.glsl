f5258595934cf6426e5939e765f70aa4995bd499
gxrj <gsouzafone@gmail.com>
2022-12-25 01:36:19 -0300

    feat: adds movement for pixel colors

--- 04-animation/shaders/elementFS.glsl
+++ 04-animation/shaders/elementFS.glsl
@@ -1,24 +1,41 @@
 #version 460
 
 out vec4 color;
+uniform float offset;
 
 void drawOneColor();
-void drawMultipleColors();
+void drawMultipleStaticColors();
+void drawMultipleDynamicColors();
+float getColorDelimiter( float );
 
 void main( ) { 
    //drawOneColor();
-   drawMultipleColors(); 
+   //drawMultipleStaticColors();
+   drawMultipleDynamicColors();
 }
 
 void drawOneColor() {
     color = vec4( 0.0, 1.0, 0.0, 1.0 );
 }
 
-void drawMultipleColors() {
+void drawMultipleStaticColors() {
     if( gl_FragCoord.x < 365 )
         color = vec4( 1.0, 0.0, 0.0, 1.0 );
     else if( gl_FragCoord.x < 428 )
         color = vec4( 0.0, 1.0, 0.0, 1.0 );
     else
         color = vec4( 0.0, 0.0, 1.0, 1.0 );
+}
+
+void drawMultipleDynamicColors() {
+    if( gl_FragCoord.x < getColorDelimiter( offset ) )
+        color = vec4( 1.0, 0.0, 0.0, 1.0 );
+    else if( gl_FragCoord.x < getColorDelimiter( offset ) + 50 )
+        color = vec4( 0.0, 1.0, 0.0, 1.0 );
+    else
+        color = vec4( 0.0, 0.0, 1.0, 1.0 );
+}
+
+float getColorDelimiter( float offset ) {
+    return offset*400 + 370;
 }
